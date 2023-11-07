a97c2311f4e55b8966d4dd239ea0a665c6912ed2
AQUADITZ <143810972+AQUADITZ@users.noreply.github.com>
2023-11-07 14:57:37 +0700

    Add files via upload

---
+++ ngoding (1)/ngoding/script.js
@@ -0,0 +1,10 @@
+let hargamobil = parseInt(prompt("Masukkan harga mobil:"));
+
+if (hargamobil < "100" && hargamobil >= "0"){
+    alert("Telah membeli mobil Ampas")
+    document.write("SELAMAT CUMA DAPAT MOBIL AMPAS!!")
+}
+else {
+    alert("Telah membeli mobil Keren" )
+    document.write("SELAMAT TELAH MEMBELI MOBIL KEREN!!")
+}
